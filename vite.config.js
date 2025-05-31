2346bbc2934dc060bc6bd8e20b2232d59967b498
Piyush Solanki <piyushsolanki2521@gmail.com>
2025-05-31 21:55:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-const GITHUB_REPO_NAME = 'Tailwindcss';
+const GITHUB_REPO_NAME = 'tailwindcss';
 export default defineConfig({
   base: `/${GITHUB_REPO_NAME}/`,
   plugins: [react(),tailwindcss(),],
